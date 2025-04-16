8d8df3bc32fdb228c64b29857bb647d82a15f625
mohammad <taskforcemmd@gmail.com>
2025-04-17 00:59:28 +0330

    create Slider in website

--- assets/main.js
+++ assets/main.js
@@ -15,3 +15,23 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
