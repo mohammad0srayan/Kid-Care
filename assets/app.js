474d3e79e953d3d9756d514b665f0c5961fdd6b5
mohammad <taskforcemmd@gmail.com>
2025-04-28 15:08:20 +0330

    Add preloader to website

--- assets/app.js
+++ assets/app.js
@@ -17,3 +17,9 @@
 //         el: '.swiper-scrollbar',
 //     },
 // });
+
+let loader = document.querySelector('.loader')
+
+window.addEventListener('load', function () {
+    loader.classList.add('hidden')
+})
